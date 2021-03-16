6bee52eb8bb436f287a5b40025f1f13d7ba360e4
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2021-03-16 15:27:55 +1100

    Added tests for igsn and oai module

--- oai/src/test/java/ardc/cerium/oai/provider/OAIProviderTest.java
+++ oai/src/test/java/ardc/cerium/oai/provider/OAIProviderTest.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { SchemaService.class })
+@TestPropertySource(properties="app.oai.enabled=true")
 public class OAIProviderTest {
 
 	@Autowired

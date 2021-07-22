35fa742a665eaa0c2f3d0022bcc1cad7deee5679
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2021-07-22 10:49:39 +1000

    RDA-236. Deprecate a test class due upcoming workflow changes

--- mycelium/src/test/java/ardc/cerium/mycelium/client/RDARegistryClientTest_Deprecated.java
+++ mycelium/src/test/java/ardc/cerium/mycelium/client/RDARegistryClientTest_Deprecated.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RDARegistryClientTest {
+class RDARegistryClientTest_Deprecated {
 
     @Test
     void getRegistryObjectByKey() {

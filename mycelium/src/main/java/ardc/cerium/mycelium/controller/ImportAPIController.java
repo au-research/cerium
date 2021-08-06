914f826f152618e63686b654c0ad1e6b54639420
Leo Monus <leo.monus@anu.edu.au>
2021-08-06 14:56:35 +1000

    IP whitelist for importer access control

--- mycelium/src/main/java/ardc/cerium/mycelium/controller/ImportAPIController.java
+++ mycelium/src/main/java/ardc/cerium/mycelium/controller/ImportAPIController.java
@@ -1,14 +1,18 @@
 package ardc.cerium.mycelium.controller;
 
 import ardc.cerium.core.common.entity.Request;
+import ardc.cerium.core.exception.ForbiddenOperationException;
 import ardc.cerium.mycelium.service.MyceliumService;
 import ardc.cerium.mycelium.task.ImportTask;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Import Controller for Mycelium service
@@ -19,6 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/services/mycelium")
 @Slf4j
 public class ImportAPIController {
+
+	@Value("${app.ip-white-list:0:0:0:0:0:0:0:1,127.0.0.1,130.56.111.120}")
+	String ipWhiteList;
 
 	// todo ? import-bulk to import multiple XML documents at the same time, wrapped?
 	// todo ? import-remote to import from a remote endpoint, ie RDA?
@@ -36,8 +43,13 @@ public class ImportAPIController {
 	 * @return something
 	 */
 	@PostMapping("/import-record")
-	public ResponseEntity<Request> importHandler(@RequestBody String json) {
-		// create new Request, store the xml
+	public ResponseEntity<Request> importHandler(@RequestBody String json, HttpServletRequest sRequest) {
+		// poor man's security until we decide the way we proceed !
+		// but most likely only allow requests from localhost is a good start
+		if(!ipWhiteList.contains(sRequest.getRemoteAddr())){
+			throw new ForbiddenOperationException(String.format("Ip %s, not authorised",sRequest.getRemoteAddr()));
+		}
+		// create new Request, store thejson payload
 		Request request = myceliumService.createImportRequest(json);
 		request.setStatus(Request.Status.ACCEPTED);
 		myceliumService.validateRequest(request);

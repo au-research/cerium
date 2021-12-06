b91676c1ed3b838994bc60c6f171b5999d844fc0
Liz Woods <lwoods@MacBook-Pro-2.local>
2021-12-06 13:29:16 +1100

    RDA-545 updated IdentifierUtil with identifier types currently formatted and added tests for resolvable values

--- mycelium/src/main/java/ardc/cerium/mycelium/util/IdentifierUtil.java
+++ mycelium/src/main/java/ardc/cerium/mycelium/util/IdentifierUtil.java
@@ -1,12 +1,68 @@
 package ardc.cerium.mycelium.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+
 public class IdentifierUtil {
 
     public static String getUrl(String identifierValue, String identifierType){
         switch (identifierType) {
 
             case "doi":
-                return String.format("https://doi.org/%s",identifierValue);
+                return String.format("https://doi.org/%s", identifierValue);
+            case "orcid":
+                return String.format("http://orcid.org/%s", identifierValue);
+            case "handle":
+                return String.format("http://hdl.handle.net/%s", identifierValue);
+            case "purl":
+                if (identifierValue.contains("http")) {
+                    return identifierValue;
+                }
+            case "AU-ANL:PEAU":
+                return String.format("http://nla.gov.au/%s", identifierValue);
+            case "igsn":
+                return String.format("http://igsn.org/%s", identifierValue);
+            case "uri":
+                return String.format("http://%s", identifierValue);
+            case "ark":
+                if (identifierValue.contains("ark:")) {
+                    return String.format("http://%s", identifierValue);
+                }
+            case "scopusID":
+                if (identifierValue.contains("www.scopus.com/")){
+                    return String.format("https://,%s", identifierValue);
+                }
+                return String.format("https://www.scopus.com/authid/detail.uri?authorId=%s", identifierValue);
+            case "grid":
+                return String.format("https://%s", identifierValue);
+            case "isni":
+                if (identifierValue.contains("isni.org/")){
+                    return String.format("http://,%s", identifierValue);
+                }
+                return String.format("http://isni.org/,%s", identifierValue);
+            case "raid":
+                identifierValue = identifierValue.toLowerCase(Locale.ROOT);
+                if(identifierValue.contains("hdl:")){
+                    identifierValue = identifierValue.substring(identifierValue.indexOf("hdl:") + 4);
+                }
+                else if(identifierValue.contains("http")){
+                    try {
+                        URL url = new URL(identifierValue);
+                        identifierValue = url.getPath().substring(1);
+                    }catch(MalformedURLException ignored){
+
+                    }
+                }
+                else if(identifierValue.contains("handle.")){
+                    try {
+                        URL url = new URL("https://" + identifierValue);
+                        identifierValue = url.getPath().substring(1);
+                    }catch(MalformedURLException ignored){
+
+                    }
+                }
+                return String.format("http://hdl.handle.net/,%s", identifierValue);
             default:
                 return null;
         }
--- mycelium/src/test/java/ardc/cerium/mycelium/util/IdentifierUtilTest.java
+++ mycelium/src/test/java/ardc/cerium/mycelium/util/IdentifierUtilTest.java
@@ -11,4 +11,59 @@ class IdentifierUtilTest {
         AssertionsForClassTypes.assertThat(url).isEqualTo("https://doi.org/10.7021/6547547");
     }
 
+    @Test
+    void it_can_get_orcid_url(){
+
+        String url = IdentifierUtil.getUrl("0000-0002-5407-9987", "orcid");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("http://orcid.org/0000-0002-5407-9987");
+    }
+
+    @Test
+    void it_can_get_handle_url(){
+
+        String url = IdentifierUtil.getUrl("11057/id59416f87", "handle");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("http://hdl.handle.net/11057/id59416f87");
+    }
+
+    @Test
+    void it_can_get_purl_url(){
+
+        String url = IdentifierUtil.getUrl("https://purl.org/au-research/grants/arc/DP0451499", "purl");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("https://purl.org/au-research/grants/arc/DP0451499");
+    }
+
+    @Test
+    void it_can_get_nla_url(){
+
+        String url = IdentifierUtil.getUrl("nla.party-1504955", "AU-ANL:PEAU");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("http://nla.gov.au/nla.party-1504955");
+    }
+
+    @Test
+    void it_can_get_igsn_url(){
+
+        String url = IdentifierUtil.getUrl("AU1243", "igsn");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("http://igsn.org/AU1243");
+    }
+
+    @Test
+    void it_can_get_uri_url(){
+
+        String url = IdentifierUtil.getUrl("communities.research.uwa.edu.au/vivo/individual/dataset458", "uri");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("http://communities.research.uwa.edu.au/vivo/individual/dataset458");
+    }
+
+    @Test
+    void it_can_get_scopusID_url(){
+
+        String url = IdentifierUtil.getUrl("55253990100", "scopusID");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("https://www.scopus.com/authid/detail.uri?authorId=55253990100");
+    }
+
+    @Test
+    void it_can_get_grid_url(){
+
+        String url = IdentifierUtil.getUrl("grid.ac/institutes/grid.1005.4", "grid");
+        AssertionsForClassTypes.assertThat(url).isEqualTo("https://grid.ac/institutes/grid.1005.4");
+    }
 }

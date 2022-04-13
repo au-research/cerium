59099377c5910da0925b6ae5585218abbd154129
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2022-04-13 14:28:46 +1000

    RDA-718. Added some internal documentation to MyceliumBackupService

--- mycelium/src/main/java/ardc/cerium/mycelium/service/MyceliumBackupService.java
+++ mycelium/src/main/java/ardc/cerium/mycelium/service/MyceliumBackupService.java
@@ -27,19 +27,32 @@ public class MyceliumBackupService {
     @Value( "${rda.backup.path:/var/data/registry-backups/}" )
     private String RDAPackupPath;
 
-    public void createBackup(String backupId, String dataSourceId) {
+	/**
+	 * Create and store a new Backup for a given data source.
+	 *
+	 *
+	 * The local graph of the DataSource Vertex will be stored at datasource.json The
+	 * local graph of every RegistryObject that belongs to that DataSource will be stored
+	 * at {id}.json where id is the RegistryObjectId
+	 * @param backupId the URL safe backup ID
+	 * @param dataSourceId the Id of the DataSource
+	 */
+	public void createBackup(String backupId, String dataSourceId) {
 
-        String backupPath = String.format(RDAPackupPath + "/%s/datasources/%s/graphs/", backupId, dataSourceId);
-        log.info("Creating backup to path {}", backupPath);
+        // backup path, /var/data/registry-backups/{backupId}/datasources/{backupId}/graphs/
+		String backupPath = String.format(RDAPackupPath + "/%s/datasources/%s/graphs/", backupId, dataSourceId);
+		log.info("Creating backup to path {}", backupPath);
 
-        File backupDirectory = new File(backupPath);
+        // create the directory if it doesn't exist
+		File backupDirectory = new File(backupPath);
         if (!backupDirectory.isDirectory()) {
             backupDirectory.mkdirs();
         }
+        // todo handle directory not created or not writable
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-        // backup data sources to datasources-graph/{id}/datasource.json
+        // backup datasource vertex to datasource.json
         Vertex datasourceVertex = graphService.getVertexByIdentifier(dataSourceId, "ds:id");
         Graph dataSourceGraph = graphService.getLocalGraph(datasourceVertex);
         try {
@@ -54,7 +67,7 @@ public class MyceliumBackupService {
             e.printStackTrace();
         }
 
-        // backup record records to datasources-graph/{id}/{registryObjectId}.json
+        // backup record vertex to {registryObjectId}.json
         try (Stream<Vertex> stream = graphService.streamRegistryObjectFromDataSource(dataSourceId)) {
             stream.forEach(vertex -> {
                 Graph graph = graphService.getLocalGraph(vertex);
@@ -73,13 +86,24 @@ public class MyceliumBackupService {
         }
     }
 
+	/**
+     * Restore a specific data source by backup Id
+     *
+     *
+	 * @param backupId
+	 * @param dataSourceId
+	 */
     public void restoreBackup(String backupId, String dataSourceId) {
+
+        // backup path, /var/data/registry-backups/{backupId}/datasources/{backupId}/graphs/
         String backupPath = String.format(RDAPackupPath + "/%s/datasources/%s/graphs/", backupId, dataSourceId);
         log.info("Restoring backup from path {}", backupPath);
 
         File backupDirectory = new File(backupPath);
         ObjectMapper objectMapper = new ObjectMapper();
 
+        // read the directory and restore the graph file for every single file within it
+        // todo parse only *.json file
         File[] files = backupDirectory.listFiles();
         Arrays.sort(files);
         for (File file : files) {
@@ -87,6 +111,11 @@ public class MyceliumBackupService {
         }
     }
 
+	/**
+     * Restore a graph file
+     *
+	 * @param file
+	 */
     public void restoreGraphFile(File file) {
         log.debug("Restoring File "+file);
         try {

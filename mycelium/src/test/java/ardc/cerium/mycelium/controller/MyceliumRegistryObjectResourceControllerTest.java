892d7673887be7551085e0dd8af6ca0a03daea42
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2022-05-19 16:16:27 +1000

    RDA-791. Moved a deprecated test

--- mycelium/src/test/java/ardc/cerium/mycelium/controller/MyceliumRegistryObjectResourceControllerTest.java
+++ mycelium/src/test/java/ardc/cerium/mycelium/controller/MyceliumRegistryObjectResourceControllerTest.java
@@ -3,13 +3,10 @@ package ardc.cerium.mycelium.controller;
 import ardc.cerium.core.common.dto.RequestDTO;
 import ardc.cerium.core.common.entity.Request;
 import ardc.cerium.core.common.model.Attribute;
-import ardc.cerium.core.common.util.Helpers;
-import ardc.cerium.mycelium.model.RegistryObject;
+import ardc.cerium.mycelium.model.mapper.RegistryObjectVertexDTOMapper;
 import ardc.cerium.mycelium.model.mapper.TreeNodeDTOMapper;
-import ardc.cerium.mycelium.rifcs.RecordState;
-import ardc.cerium.mycelium.service.MyceliumIndexingService;
+import ardc.cerium.mycelium.model.mapper.VertexDTOMapper;
 import ardc.cerium.mycelium.service.MyceliumService;
-import ardc.cerium.mycelium.service.MyceliumSideEffectService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,26 +18,28 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.UUID;
-
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@WebMvcTest(MyceliumServiceController.class)
-class MyceliumServiceControllerTest {
+@WebMvcTest(MyceliumRegistryObjectResourceController.class)
+class MyceliumRegistryObjectResourceControllerTest {
 
-	public static String END_POINT = "/api/services/mycelium/";
-
-	public static String IMPORT_ENDPOINT = END_POINT + "import-record";
+	public static String RESOURCE_ENDPOINT = "/api/resources/mycelium-registry-objects";
 
 	@MockBean
 	MyceliumService myceliumService;
 
 	@MockBean
 	TreeNodeDTOMapper treeNodeDTOMapper;
+
+	@MockBean
+	RegistryObjectVertexDTOMapper registryObjectVertexDTOMapper;
+
+	@MockBean
+	VertexDTOMapper vertexDTOMapper;
 
 	@Autowired
 	MockMvc mockMvc;
@@ -53,7 +52,7 @@ class MyceliumServiceControllerTest {
 		mockedRequest.setAttribute(Attribute.PAYLOAD_PATH, scenario1Path);
 		when(myceliumService.createRequest(any(RequestDTO.class))).thenReturn(mockedRequest);
 
-		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(IMPORT_ENDPOINT)
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(RESOURCE_ENDPOINT)
 				.accept(MediaType.APPLICATION_JSON);
 
 		mockMvc.perform(request).andExpect(status().isBadRequest());
